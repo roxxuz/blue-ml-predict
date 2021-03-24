d76012b7f59894c512b4f3b4fc68655d00dbd198
swardh <73133546+swardh@users.noreply.github.com>
2021-03-24 16:32:07 +0100

    comments main.py

--- main.py
+++ main.py
@@ -6,11 +6,16 @@ from tensorflow.compat.v1.keras.models import load_model
 from flask import Flask, request, redirect, url_for, render_template
 from werkzeug.utils import secure_filename
 
+#Create Flask instance
 app = Flask(__name__)
 
-global model
+#Loding pretrained Tensorflow model
 model = load_model('models/blue-tf-img-pred-model.h5')
 
+
+#Default endpoint. App route defines what will happen when client visits the main page both for "GET" and "POST" methods.
+#If method is "POST" the file is saved in the uploads folder and the client is redirected to /prediction/(uploaded filename)
+#Else (if method is "GET") send user to index.html
 @app.route('/', methods=['GET', 'POST'])
 def main_page():
    if request.method == 'POST':
@@ -20,11 +25,15 @@ def main_page():
       return redirect(url_for('prediction', filename=filename))
    return render_template('index.html')
 
+#App route defines what will happen when client visits /prediction/(uploaded filename)
+#Image is read from the uploads folder using the filename from the created url.
+#Image is being resized to 32*32 pixels.
+#Predicted the image with our pretrained model
 
 @app.route('/prediction/<filename>')
 def prediction(filename):
    image = plt.imread(os.path.join('uploads', filename))
-   image_resized = resize(image, (32, 32, 3)) 
+   image_resized = resize(image, (32, 32, 3))
    probabilities = model.predict(np.array([image_resized, ]))[0, :]
    classes = ['Airplane', 'Car', 'Bird',
                'Cat', 'Deer', 'Dog', 'Frog', 'Horse', 'Ship', 'Truck']
@@ -40,4 +49,4 @@ def prediction(filename):
    return render_template('predict.html', predictions=predictions)
 
 
-app.run(host='127.0.0.1', port=80)
+app.run(host='127.0.0.1', port=8080, debug=True)
---
+++ resize.py
@@ -0,0 +1,97 @@
+from PIL import Image
+import math
+import numpy as np
+import os
+import requests
+
+
+url = None
+
+def getfromurl(url):
+    try:
+        r = requests.get(url)
+        with open("images/x.jpg", "wb") as f:
+            f.write(r.content)
+    except:
+        pass
+
+
+def deletefile():
+    for i in os.listdir("images"):
+        os.remove(f"images/{i}")
+
+
+#def get_np_array(image):
+#    ###GET RESIZED IMAGE AS NP ARRAY
+#    image_as_array.append(image)
+
+
+def resize(path, size_width=32, size_height=32, save_image=False, savepath="f{path}/resized"):
+
+    image_as_array = []
+
+    if save_image == True:
+        try:
+            os.mkdir(savepath)
+        except:
+            pass
+    else:
+        pass
+
+    for i in os.listdir(path):
+
+        try:
+            image = Image.open(f"{path}/{i}")
+            filename = i
+            ### GET WIDTH AND HEIGHT FROM IMAGE
+            width, height = image.size
+            print(width, height)
+
+            ### DIFF GETS DIFFERENCE BETWEEN WIDTH AND HEIGHT
+            diff = width - height
+            print(f"difference {diff}")
+
+            ### SUPERADVANCED MATH STUFF (MAKING SURE YOU GET A POSITIVE VALUE)
+
+            exp_diff = int(diff**2)
+            print(f"Exponent {exp_diff}")
+            squared_diff = int(math.sqrt(exp_diff))
+            print(f"Squared difference {squared_diff}")
+
+            ### GET HALF THE DIFFERENCE TO CENTER THE CROP
+
+            half_crop = int(squared_diff/2)
+            print(f"Half difference {half_crop}")
+
+            print(half_crop, 0, width-half_crop, height)
+            print(0, half_crop, 0, half_crop+height)
+
+
+            if width > height:
+                box = (half_crop, 0, width-half_crop, height)
+            else:
+                box = (0, half_crop, width, height-half_crop)
+
+
+            cropped_image = image.crop(box)
+            print(f"cropped size {cropped_image.size}")
+            #x = cropped_image.thumbnail((32, 32))
+            resized_image = cropped_image.resize((size_width, size_height))
+            print(f"resized {resized_image.size}")
+
+#            if save_image == True:
+#                ###SAVE IMAGE IN NEW FOLDER AND ADD RESIZED TO NAME
+#                resized_image.save(savepath+"resized"+filename)
+#            else:
+#                pass
+
+            ###GET RESIZED IMAGE AS NP ARRAY
+            image_as_array.append(np.asarray(resized_image))
+            np_array = np.asarray(image_as_array)
+            np_array = np_array/255
+            print(type(np_array))
+
+            return np_array
+
+        except:
+            print("Something went wrong")
